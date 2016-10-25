6a4d55b98a6cca560c23dc426e40e9fc3c91d348
Qiu Juntao <juntao.qiu@gmail.com>
2016-10-26 00:27:20 +1100

    add some style

--- viz.rb
+++ viz.rb
@@ -3,6 +3,11 @@ require 'graphviz'
 
 graph = GraphViz.new(:G, :type => :digraph)
 
+graph.graph["fontname"] = "PT Mono"
+graph.edge['color'] = '#05c3de'
+graph.node['color'] = '#3C7CCC'
+graph.node['shape'] = 'record'
+
 xlsx = Roo::Excelx.new('./U2000-1121.xlsx')
 xlsx.each_row_streaming(offset: 1) do |row|
     graph.add_nodes(row[0].value)
