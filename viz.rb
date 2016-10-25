2a0fd2344b8efadc580df8d895621f29f2a0a0d6
Qiu Juntao <juntao.qiu@gmail.com>
2016-10-26 00:06:25 +1100

    make it work as the first prototype

--- viz.rb
+++ viz.rb
@@ -1,10 +1,15 @@
 require 'roo'
+require 'graphviz'
 
-# xlsx = Roo::Spreadsheet.open('./U2000-1121.xlsx', extension: :xlsx)
-# p xlsx.info
-# p xlsx.sheets
+graph = GraphViz.new(:G, :type => :digraph)
 
 xlsx = Roo::Excelx.new('./U2000-1121.xlsx')
 xlsx.each_row_streaming(offset: 1) do |row|
-    p row
+    graph.add_nodes(row[0].value)
+
+    if(row[4]) 
+        graph.add_edges(row[4].value, row[0].value)
+    end
 end
+
+graph.output(:png => 'U2000-1121.png')
